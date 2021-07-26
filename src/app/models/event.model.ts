2ec3d96692f13cd4b5b2c206cef70054c44fd9a2
NinjaKate <38056303+NinjaKate@users.noreply.github.com>
2021-07-26 23:47:27 +0300

    tables update

--- src/app/models/event.model.ts
+++ src/app/models/event.model.ts
@@ -3,7 +3,5 @@ import { Dictionary } from "./dictionary.model";
 export interface Event {
   name: string;
   date: Date;
-  timeHours: number;
-  timeMinutes: number;
   trainer: Dictionary;
 }
--- src/app/services/trainings.service.ts
+++ src/app/services/trainings.service.ts
@@ -18,6 +18,397 @@ export class TrainingsService {
       participants: [
         {
           group: {
+            value: 'Обучение',
+            option: 'option3',
+          },
+          status: {
+            value: 'Менеджер по персоналу',
+            option: 'option1',
+          },
+          experience: {
+            value: 'Более 10 лет',
+            option: 'option3',
+          },
+          subdivision: {
+            value: 'Администрация',
+            option: 'option1',
+          },
+          area: {
+            value: 'Сибирский',
+            option: 'option3',
+          },
+          city: {
+            value: 'Иркутск',
+            option: 'option3',
+          },
+          name: 'Юлия Грунина',
+          id: 2,
+        },
+        {
+          group: {
+            value: 'Управление',
+            option: 'option2',
+          },
+          status: {
+            value: 'Системный администратор',
+            option: 'option3',
+          },
+          experience: {
+            value: '3-10 лет',
+            option: 'option2',
+          },
+          subdivision: {
+            value: 'Администрация',
+            option: 'option1',
+          },
+          area: {
+            value: 'Центральный',
+            option: 'option1',
+          },
+          city: {
+            value: 'Белгород',
+            option: 'option4',
+          },
+          name: 'Светлана Иванова',
+          id: 4,
+        },
+        {
+          group: {
+            value: 'Администрирование',
+            option: 'option1',
+          },
+          status: {
+            value: 'Менеджер по персоналу',
+            option: 'option1',
+          },
+          experience: {
+            value: '3-10 лет',
+            option: 'option2',
+          },
+          subdivision: {
+            value: 'Администрация',
+            option: 'option1',
+          },
+          area: {
+            value: 'Центральный',
+            option: 'option1',
+          },
+          city: {
+            value: 'Белгород',
+            option: 'option4',
+          },
+          name: 'Максим Вильниц',
+          id: 0,
+        },
+        {
+          group: {
+            value: 'Администрирование',
+            option: 'option1',
+          },
+          status: {
+            value: 'Менеджер по продажам',
+            option: 'option2',
+          },
+          experience: {
+            value: '1-3 года',
+            option: 'option1',
+          },
+          subdivision: {
+            value: 'Администрация',
+            option: 'option1',
+          },
+          area: {
+            value: 'Северо-Западный',
+            option: 'option2',
+          },
+          city: {
+            value: 'Санкт-Петербург',
+            option: 'option2',
+          },
+          name: 'Ольга Крышкова',
+          id: 1,
+        },
+      ],
+      events: [
+        {
+          name: 'Страховой случай или как помочь клиенту',
+          date: new Date(),
+          trainer: {
+            value: 'Тим Кук',
+            option: 'option3',
+          }
+        },
+        {
+          name: 'Главные правила продаж',
+          date: new Date(),
+          trainer: {
+            value: 'Сергей Ефремов',
+            option: 'option1',
+          }
+        },
+        {
+          name: 'Страхование без проблем',
+          date: new Date(),
+          trainer: {
+            value: 'Илон Маск',
+            option: 'option2',
+          }
+        },
+      ],
+      progress: 90,
+      id: 0,
+    },
+    {
+      program: {
+        value: 'Главные правила продаж',
+        option: 'option2',
+      },
+      conditionCode: 'registration',
+      participants: [
+        {
+          group: {
+            value: 'Обучение',
+            option: 'option3',
+          },
+          status: {
+            value: 'Менеджер по персоналу',
+            option: 'option1',
+          },
+          experience: {
+            value: 'Более 10 лет',
+            option: 'option3',
+          },
+          subdivision: {
+            value: 'Администрация',
+            option: 'option1',
+          },
+          area: {
+            value: 'Сибирский',
+            option: 'option3',
+          },
+          city: {
+            value: 'Иркутск',
+            option: 'option3',
+          },
+          name: 'Юлия Грунина',
+          id: 2,
+        },
+        {
+          group: {
+            value: 'Администрирование',
+            option: 'option1',
+          },
+          status: {
+            value: 'Менеджер по продажам',
+            option: 'option2',
+          },
+          experience: {
+            value: '1-3 года',
+            option: 'option1',
+          },
+          subdivision: {
+            value: 'Администрация',
+            option: 'option1',
+          },
+          area: {
+            value: 'Северо-Западный',
+            option: 'option2',
+          },
+          city: {
+            value: 'Санкт-Петербург',
+            option: 'option2',
+          },
+          name: 'Ольга Крышкова',
+          id: 1,
+        },
+      ],
+      events: [
+        {
+          name: 'Страхование без проблем',
+          date: new Date(),
+          trainer: {
+            value: 'Илон Маск',
+            option: 'option2',
+          }
+        },
+        {
+          name: 'Главные правила продаж',
+          date: new Date(),
+          trainer: {
+            value: 'Сергей Ефремов',
+            option: 'option1',
+          }
+        },
+        {
+          name: 'Страховой случай или как помочь клиенту',
+          date: new Date(),
+          trainer: {
+            value: 'Тим Кук',
+            option: 'option3',
+          }
+        }
+      ],
+      progress: 92,
+      id: 1,
+    },
+    {
+      program: {
+        value: 'Первичное обучение КС',
+        option: 'option3',
+      },
+      conditionCode: 'name',
+      participants: [
+        {
+          group: {
+            value: 'Обучение',
+            option: 'option3',
+          },
+          status: {
+            value: 'Менеджер по персоналу',
+            option: 'option1',
+          },
+          experience: {
+            value: 'Более 10 лет',
+            option: 'option3',
+          },
+          subdivision: {
+            value: 'Администрация',
+            option: 'option1',
+          },
+          area: {
+            value: 'Сибирский',
+            option: 'option3',
+          },
+          city: {
+            value: 'Иркутск',
+            option: 'option3',
+          },
+          name: 'Юлия Грунина',
+          id: 2,
+        },
+        {
+          group: {
+            value: 'Управление',
+            option: 'option2',
+          },
+          status: {
+            value: 'Системный администратор',
+            option: 'option3',
+          },
+          experience: {
+            value: '3-10 лет',
+            option: 'option2',
+          },
+          subdivision: {
+            value: 'Администрация',
+            option: 'option1',
+          },
+          area: {
+            value: 'Центральный',
+            option: 'option1',
+          },
+          city: {
+            value: 'Белгород',
+            option: 'option4',
+          },
+          name: 'Светлана Иванова',
+          id: 4,
+        },
+      ],
+      events: [
+        {
+          name: 'Страховой случай или как помочь клиенту',
+          date: new Date(),
+          trainer: {
+            value: 'Тим Кук',
+            option: 'option3',
+          }
+        },
+        {
+          name: 'Главные правила продаж',
+          date: new Date(),
+          trainer: {
+            value: 'Сергей Ефремов',
+            option: 'option1',
+          }
+        },
+        {
+          name: 'Страхование без проблем',
+          date: new Date(),
+          trainer: {
+            value: 'Илон Маск',
+            option: 'option2',
+          }
+        },
+      ],
+      progress: 82,
+      id: 2,
+    },
+    {
+      program: {
+        value: 'Вторичное обучение КС',
+        option: 'option4',
+      },
+      conditionCode: 'nothing',
+      participants: [
+        {
+          group: {
+            value: 'Управление',
+            option: 'option2',
+          },
+          status: {
+            value: 'Системный администратор',
+            option: 'option3',
+          },
+          experience: {
+            value: '3-10 лет',
+            option: 'option2',
+          },
+          subdivision: {
+            value: 'Администрация',
+            option: 'option1',
+          },
+          area: {
+            value: 'Центральный',
+            option: 'option1',
+          },
+          city: {
+            value: 'Белгород',
+            option: 'option4',
+          },
+          name: 'Светлана Иванова',
+          id: 4,
+        },
+        {
+          group: {
+            value: 'Администрирование',
+            option: 'option1',
+          },
+          status: {
+            value: 'Менеджер по продажам',
+            option: 'option2',
+          },
+          experience: {
+            value: '1-3 года',
+            option: 'option1',
+          },
+          subdivision: {
+            value: 'Администрация',
+            option: 'option1',
+          },
+          area: {
+            value: 'Северо-Западный',
+            option: 'option2',
+          },
+          city: {
+            value: 'Санкт-Петербург',
+            option: 'option2',
+          },
+          name: 'Ольга Крышкова',
+          id: 1,
+        },
+        {
+          group: {
             value: 'Администрирование',
             option: 'option1',
           },
@@ -49,134 +440,6 @@ export class TrainingsService {
         {
           name: 'Главные правила продаж',
           date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
-          trainer: {
-            value: 'Сергей Ефремов',
-            option: 'option1',
-          }
-        },
-        {
-          name: 'Страхование без проблем',
-          date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
-          trainer: {
-            value: 'Илон Маск',
-            option: 'option2',
-          }
-        },
-        {
-          name: 'Страховой случай или как помочь клиенту',
-          date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
-          trainer: {
-            value: 'Тим Кук',
-            option: 'option3',
-          }
-        }
-      ],
-      progress: 90,
-      id: 0,
-    },
-    {
-      program: {
-        value: 'Главные правила продаж',
-        option: 'option2',
-      },
-      conditionCode: 'registration',
-      participants: [],
-      events: [
-        {
-          name: 'Главные правила продаж',
-          date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
-          trainer: {
-            value: 'Сергей Ефремов',
-            option: 'option1',
-          }
-        },
-        {
-          name: 'Страхование без проблем',
-          date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
-          trainer: {
-            value: 'Илон Маск',
-            option: 'option2',
-          }
-        },
-        {
-          name: 'Страховой случай или как помочь клиенту',
-          date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
-          trainer: {
-            value: 'Тим Кук',
-            option: 'option3',
-          }
-        }
-      ],
-      progress: 92,
-      id: 1,
-    },
-    {
-      program: {
-        value: 'Первичное обучение КС',
-        option: 'option3',
-      },
-      conditionCode: 'name',
-      participants: [],
-      events: [
-        {
-          name: 'Страховой случай или как помочь клиенту',
-          date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
-          trainer: {
-            value: 'Тим Кук',
-            option: 'option3',
-          }
-        },
-        {
-          name: 'Главные правила продаж',
-          date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
-          trainer: {
-            value: 'Сергей Ефремов',
-            option: 'option1',
-          }
-        },
-        {
-          name: 'Страхование без проблем',
-          date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
-          trainer: {
-            value: 'Илон Маск',
-            option: 'option2',
-          }
-        }
-      ],
-      progress: 82,
-      id: 2,
-    },
-    {
-      program: {
-        value: 'Вторичное обучение КС',
-        option: 'option4',
-      },
-      conditionCode: 'nothing',
-      participants: [],
-      events: [
-        {
-          name: 'Главные правила продаж',
-          date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
           trainer: {
             value: 'Сергей Ефремов',
             option: 'option1',
@@ -185,8 +448,6 @@ export class TrainingsService {
         {
           name: 'Страховой случай или как помочь клиенту',
           date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
           trainer: {
             value: 'Тим Кук',
             option: 'option3',
@@ -195,8 +456,6 @@ export class TrainingsService {
         {
           name: 'Страхование без проблем',
           date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
           trainer: {
             value: 'Илон Маск',
             option: 'option2',
@@ -218,8 +477,6 @@ export class TrainingsService {
         {
           name: 'Главные правила продаж',
           date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
           trainer: {
             value: 'Сергей Ефремов',
             option: 'option1',
@@ -228,8 +485,6 @@ export class TrainingsService {
         {
           name: 'Страховой случай или как помочь клиенту',
           date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
           trainer: {
             value: 'Тим Кук',
             option: 'option3',
@@ -238,8 +493,6 @@ export class TrainingsService {
         {
           name: 'Страхование без проблем',
           date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
           trainer: {
             value: 'Илон Маск',
             option: 'option2',
@@ -290,8 +543,6 @@ export class TrainingsService {
         {
           name: 'Страхование без проблем',
           date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
           trainer: {
             value: 'Илон Маск',
             option: 'option2',
@@ -300,8 +551,6 @@ export class TrainingsService {
         {
           name: 'Главные правила продаж',
           date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
           trainer: {
             value: 'Сергей Ефремов',
             option: 'option1',
@@ -310,8 +559,6 @@ export class TrainingsService {
         {
           name: 'Страховой случай или как помочь клиенту',
           date: new Date(),
-          timeHours: new Date().getHours(),
-          timeMinutes: new Date().getMinutes(),
           trainer: {
             value: 'Тим Кук',
             option: 'option3',
@@ -350,6 +597,137 @@ export class TrainingsService {
       ],
       progress: 92,
       id: 11,
+    },
+    {
+      program: {
+        value: 'Первичное обучение КС',
+        option: 'option3',
+      },
+      events: [
+        {
+          name: 'Страховой случай или как помочь клиенту',
+          date: new Date(),
+          trainer: {
+            value: 'Тим Кук',
+            option: 'option3',
+          }
+        },
+        {
+          name: 'Страхование без проблем',
+          date: new Date(),
+          trainer: {
+            value: 'Илон Маск',
+            option: 'option2',
+          }
+        },
+        {
+          name: 'Главные правила продаж',
+          date: new Date(),
+          trainer: {
+            value: 'Сергей Ефремов',
+            option: 'option1',
+          }
+        },
+      ],
+      conditionCode: 'registration',
+      participants: [
+        {
+          group: {
+            value: 'Обучение',
+            option: 'option3',
+          },
+          status: {
+            value: 'Менеджер по персоналу',
+            option: 'option1',
+          },
+          experience: {
+            value: 'Более 10 лет',
+            option: 'option3',
+          },
+          subdivision: {
+            value: 'Администрация',
+            option: 'option1',
+          },
+          area: {
+            value: 'Сибирский',
+            option: 'option3',
+          },
+          city: {
+            value: 'Иркутск',
+            option: 'option3',
+          },
+          name: 'Юлия Грунина',
+          id: 2,
+        },
+      ],
+      progress: 84,
+      id: 12,
+    },
+    {
+      program: {
+        value: 'Страхование с заботой о клиенте',
+        option: 'option1',
+      },
+      events: [
+        {
+          name: 'Главные правила продаж',
+          date: new Date(),
+          trainer: {
+            value: 'Сергей Ефремов',
+            option: 'option1',
+          }
+        },
+        {
+          name: 'Страховой случай или как помочь клиенту',
+          date: new Date(),
+          trainer: {
+            value: 'Тим Кук',
+            option: 'option3',
+          }
+        },
+        {
+          name: 'Страхование без проблем',
+          date: new Date(),
+          trainer: {
+            value: 'Илон Маск',
+            option: 'option2',
+          }
+        }
+
+      ],
+      conditionCode: 'no-code',
+      participants: [
+        {
+          group: {
+            value: 'Администрирование',
+            option: 'option1',
+          },
+          status: {
+            value: 'Менеджер по персоналу',
+            option: 'option1',
+          },
+          experience: {
+            value: '3-10 лет',
+            option: 'option2',
+          },
+          subdivision: {
+            value: 'Администрация',
+            option: 'option1',
+          },
+          area: {
+            value: 'Центральный',
+            option: 'option1',
+          },
+          city: {
+            value: 'Москва',
+            option: 'option1',
+          },
+          name: 'Марат Большаков',
+          id: 3,
+        },
+      ],
+      progress: 75,
+      id: 13,
     },
   ];
 
